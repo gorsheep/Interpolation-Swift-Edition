f61968ea7a36981cfb6b22c5efdb479bee812eb2
Даниил Волошин <kuravasic@gmail.com>
2018-07-10 17:54:12 +0300

    Initial Commit

---
+++ Interpolation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Interpolation
+//
+//  Created by Даниил Волошин on 7/10/18.
+//  Copyright © 2018 Даниил Волошин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
